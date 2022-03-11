095d579cbba3ba3490eb42122ef2aa471061394a
tijanimukhtarakande <tijanimukhtarr@gmail.com>
2022-03-11 16:38:38 +0100

    more

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the size of various types
  * return: 0 if exited properly, non-zero otherwise
-*/
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
