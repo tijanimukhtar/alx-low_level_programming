4df71d04bdec458a444b02ca05d520e81a7c5fad
tijanimukhtarakande <tijanimukhtarr@gmail.com>
2022-03-13 16:27:22 +0000

    more fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ int main(void)
 
 	while (e < 58)
 	{
-		i = 48'
+		i = 48;
 		while (i < 58)
 		{
 			if (e != i && e < i)
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible different combinations of three digits
- * Return: Always 0 (Succss)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l > m && m > n)
+			for (l =50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
